e0a60d1d00b5a348ece8d510ac6b1a710fbc2288
MirandaLi1020 <lisirui19881020@gmail.com>
2018-01-15 00:55:49 +1100

    update db

--- assistant/db.py
+++ assistant/db.py
@@ -3,13 +3,16 @@ from bson.objectid import ObjectId
 
 class DB():
     def __init__(self):
-        self.client = MongoClient('mongodb://localhost:27017/')
+        mongoConnectionStr = 'mongodb://localhost:27017/'
+        if os.environ.get('APP_LOCATION') == 'heroku':
+            mongoConnectionStr = 'mongodb://mirandaLi:lisirui1020@cluster0-shard-00-00-eluxi.mongodb.net:27017,cluster0-shard-00-01-eluxi.mongodb.net:27017,cluster0-shard-00-02-eluxi.mongodb.net:27017/admin?readPreference=primary&ssl=true'
+        self.client = MongoClient(self.mongoConnectionStr)
         self.db = self.client['wacp-dev']
         self.LoginInfo = self.db.loginInfo
         self.Messages = self.db.Messages
         self.User = self.db.User
     def get_login_info_by_uuid(self, uuid):
-        client = MongoClient('mongodb://localhost:27017/')
+        client = MongoClient(self.mongoConnectionStr)
         db = client['wacp-dev']
         return db.loginInfo.find_one({'uuid': uuid})
     def create_user(self,userInfo):
